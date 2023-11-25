ef98bc76ddc639bba00d1aab93b1f2df2d12863b
Osama Hardan <90199470+ohardan2@users.noreply.github.com>
2023-11-25 12:59:42 +0300

    fix:user not defined

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,15 +2,18 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { readConferencesAction } from "@/app/actions";
+import { redirect } from "next/navigation";
 
 export default function Dashboard() {
   const [conferences, setConferences] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     if (!user) {
-      window.location.href = "/login";
+      redirect("/forms/login");
     }
+    setUser(user);
     readConferencesAction(user.id).then((result) => {
       if (result.error === 0) {
         setConferences(result.payload);
@@ -20,7 +23,9 @@ export default function Dashboard() {
 
   return (
     <>
-      <Link href={`/forms/register-conference?organizerId=${user.id}`}>Register New Conference</Link>
+      <Link href={`/forms/register-conference?organizerId=${user?.id}`}>
+        Register New Conference
+      </Link>
       <p className="text-7xl">Dashboard</p>
       <Conferences conferences={conferences} />
     </>
